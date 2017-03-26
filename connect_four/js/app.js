b18d5c025f92bfddda488bf3a8f7380df38e5bea
Geraldine Morales <geraldine.morales02@gmail.com>
2017-03-25 23:41:54 -0400

    made adjustments to css for game/page layout

--- connect_four/js/app.js
+++ connect_four/js/app.js
@@ -8,6 +8,25 @@ $(function(){ //start window onload
 }); //end window onload
 
 for (i=1; i <= 42; i++){
-  $gameSquares = $('<div>').addClass('game-squares');
-  $gameSquares.appendTo('#board');
+  var $gameCircles = $('<div>').attr('id', 'circle' + i);
+  $gameCircles.addClass('game-circles');
+  $gameCircles.appendTo('#board');
 }
+var playerTurn = "salmon";
+
+var discColor = function(obj){
+        {
+            if (playerTurn == "salmon")
+            {
+                obj.style.background = "lightsalmon";
+                obj.style.border = "1px solid #d3d3d3";
+            }
+            else
+            {
+                obj.style.backgroundColor = "lightskyblue";
+                obj.style.border = "1px solid #d3d3d3";
+            }
+        }
+      };
+
+  discColor();
