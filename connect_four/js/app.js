1f3725e54bc9341a5625648fded605ca6b75db8c
Geraldine Morales <geraldine.morales02@gmail.com>
2017-03-26 22:45:01 -0400

    cleaned up code

--- connect_four/js/app.js
+++ connect_four/js/app.js
@@ -2,17 +2,21 @@ console.log('connect-four is now connected!');
 
 $(function(){ //start window onload
 
-  //event listeners go here
 
   //event listener
   $('.game-circles').on('click', circlesClicked);
 
 }); //end window onload
 
+//===================================
+// Global Variables
+//===================================
 var salmon = 'lightsalmon';
 var blue = 'lightskyblue';
 var toggle = true;
 
+
+//create divs for gameboard
 for (i=1; i <= 42; i++){
   var $gameCircles = $('<div>').attr('id', i);
   $gameCircles.addClass('game-circles');
@@ -21,22 +25,26 @@ for (i=1; i <= 42; i++){
 
 
 
-
+//===================================
+// Event Handler
+//===================================
 var circlesClicked = function(){
 
   var $discClicked = $(this).attr('id');
   console.log($discClicked);
-  // var $discNumber = $discClicked - 1;
-  // var $discAboveId = $(this).attr('id') - 7;
+  var $discAboveId = $(this).attr('id') - 7;
   // var $discAbove = $(this).next('#' + $discAboveId);
-
+  // if (number below has color){
+  //   fill this in
+  // } else
+  // dont
 
   if($(this).attr('id') > 35 && toggle === true){
-    $("#" + $discClicked).css('background-color', 'lightsalmon');
+    $("#" + $discClicked).css('background-color', 'lightsalmon').text('p1');
     toggle = false;
     $(this).off('click', circlesClicked);
   } else if($(this).attr('id') > 35){
-    $("#" + $discClicked).css('background-color', 'lightskyblue');
+    $("#" + $discClicked).css('background-color', 'aquamarine').text('p2');
     toggle = true;
     $(this).off('click', circlesClicked);
   }
